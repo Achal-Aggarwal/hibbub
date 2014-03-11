ef28df23b75704c830a85422e3a5b59641bea961
Achal-Aggarwal <theachalaggarwal@gmail.com>
2014-03-12 02:10:14 +0530

    Fix tokeniser test executer for content model flag change and segfault on no doctype name.

--- test/testutils.h
+++ test/testutils.h
@@ -63,6 +63,7 @@ typedef bool (*line_func)(const char *data, size_t datalen, void *pw);
 static size_t parse_strlen(const char *str, size_t limit);
 bool parse_testfile(const char *filename, line_func callback, void *pw);
 size_t parse_filesize(const char *filename);
+size_t n_str(const char *str);
 
 /**
  * Testcase datafile parser driver
@@ -143,6 +144,24 @@ size_t parse_filesize(const char *filename)
 	len = ftell(fp);
 
 	fclose(fp);
+
+	return len;
+}
+
+/**
+ * Utility string length measurer; assumes strings are '\0' terminated
+ *
+ * \param str    String to measure length of
+ * \return String length
+ */
+size_t n_str(const char *str)
+{
+	size_t len = 0;
+
+	if (str == NULL)
+		return 0;
+
+	for (; *str++;len++);
 
 	return len;
 }
